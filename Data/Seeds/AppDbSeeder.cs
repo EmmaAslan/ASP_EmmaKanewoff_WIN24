163bf7b9a35f7447afb3d691ec2a554fd29d3377
Emma Kanewoff <emmakanewoff@gmail.com>
2025-04-14 14:19:00 +0200

    Migration, Seeds, Partials

--- Data/Seeds/AppDbSeeder.cs
+++ Data/Seeds/AppDbSeeder.cs
@@ -26,5 +26,41 @@ public static class AppDbSeeder
        
     }
 
+    public static async Task SeedProjects(IServiceProvider services)
+    {
+        var db = services.GetRequiredService<DataContext>();
+
+        if (!db.Projects.Any())
+        {
+            var projects = new List<ProjectEntity>
+            {
+                new() { 
+                    ProjectName = "Website Redesign", 
+                    ClientName = "GitLab Inc.", 
+                    Description = "It is necessary to develop a website redesign in a corporate style.",
+                    StartDate = new DateTime(2024, 12, 1),
+                    EndDate = new DateTime(2025, 3, 31),
+                    Budget = 68500.00m, 
+                    StatusId = 1
+                },
+                new() {
+                    ProjectName = "Landing Page",
+                    ClientName = "Bitbucket, Inc.",
+                    Description = "It is necessary to create a landing together with the development of design.",
+                    StartDate = new DateTime(2025, 2, 15),
+                    EndDate = null,
+                    Budget = 68500.00m,
+                    StatusId = 2 
+                },
+
+            };
+
+            await db.Projects.AddRangeAsync(projects);
+            await db.SaveChangesAsync();
+        }
+
+
+    }
+
 
 }
---
+++ Domain/Models/Project.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Models;
+
+public class Project
+{
+    public string Id { get; set; } = null!;
+    public string ProjectName { get; set; } = null!;
+    public string ClientName { get; set; } = null!;
+    public string? Description { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public decimal? Budget { get; set; }
+    //public Status Status { get; set; } = null!;
+}
