b44331d5c1815d471693e5ecdd4c62a29c1ba381
Emma Kanewoff <emmakanewoff@gmail.com>
2025-05-02 14:42:40 +0200

    Fixed "required"-message

--- Business/Models/SignUpFormData.cs
+++ Business/Models/SignUpFormData.cs
@@ -6,27 +6,28 @@ public class SignUpFormData
 {
     [Display(Name = "First Name", Prompt = "Enter first name")]
     [DataType(DataType.Text)]
-    [Required]
+    [Required(ErrorMessage = "Required.")]
     public string FirstName { get; set; } = null!;
 
     [Display(Name = "Last Name", Prompt = "Enter last name")]
     [DataType(DataType.Text)]
-    [Required]
+    [Required(ErrorMessage = "Required.")]
     public string LastName { get; set; } = null!;
 
 
     [Display(Name = "Email", Prompt = "Enter email address")]
     [DataType(DataType.EmailAddress)]
-    [Required]
+    [Required(ErrorMessage = "Required.")]
     public string Email { get; set; } = null!;
 
     [Display(Name = "Password", Prompt = "Enter password")]
     [DataType(DataType.Password)]
-    [Required]
+    [Required(ErrorMessage = "Required.")]
     public string Password { get; set; } = null!;
 
     [Display(Name = "Confirm Password", Prompt = "Confirm password")]
     [DataType(DataType.Password)]
+    [Required(ErrorMessage = "Required.")]
     [Compare(nameof(Password), ErrorMessage = "Passwords don't match!")]
     public string ConfirmPassword { get; set; } = null!;
 
